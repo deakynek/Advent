e9d927f2c2baa3c4e73fbde725750b24b81db60d
BrilliantGambit <deakynek@gmail.com>
2019-12-08 20:57:01 -0600

    Cleaning up Advent 8

--- Advent8.ps1
+++ Advent8.ps1
@@ -1,33 +1,18 @@
-﻿function PerformOperation
-{
-	Param(
-		[Parameter(Mandatory=$true)]
-		[String[]] $array,
-		
-		[Parameter(Mandatory=$true)]
-		[int] $commandIndex
-		)
-
-}
-
-$file = (Get-Content Advent8_input.txt).ToString()
+﻿$file = (Get-Content Advent8_input.txt).ToString()
 $Height = 6
 $Width = 25
-
 $Area = $Height * $Width
 
 $Layers = @()
-
 for($i = 0; $i -lt $file.Length/$Area; $i++)
 {
     $Layers += $file.Substring($Area*$i,$Area)
 }
 
-
+<#Part 1#>
 $ZEROS = $null
 $Ones = $null
 $Twos = $null
-
 foreach ($lay in $Layers)
 {
     $Matches = Select-String -InputObject $lay -Pattern "0" -AllMatches
@@ -36,15 +21,11 @@ foreach ($lay in $Layers)
     if ($ZEROS -eq $null -or $count -lt $ZEROS )
     {
         $ZEROS = $count
-        Write-Host "Zeros:" $ZEROS
         $Matches = Select-String -InputObject $lay -Pattern "1" -AllMatches
         $Ones = $Matches.Matches.Count
-        
-        Write-Host "Ones:" $Ones
+		
         $Matches = Select-String -InputObject $lay -Pattern "2" -AllMatches
         $Twos = $Matches.Matches.Count
-        
-        Write-Host "Twos:" $Twos
     }
 
 }
@@ -52,14 +33,14 @@ foreach ($lay in $Layers)
 Write-Host "Min zeros: " $ZEROS
 Write-Host "On the ones and twos:" ($Ones*$Twos)
 
-
+<#Part 2#>
 $Final = [System.Collections.ArrayList](0..($Layers[0].Length-1))
 $TotalIndexes = @(0..($Layers[0].Length-1))
 $UsedIndexes = @()
 
+<#Find Result of Overlapped Layers#>
 foreach ($lay in $Layers)
 {
-    
     foreach($index in $TotalIndexes | Where-Object{$UsedIndexes -NotContains $_})
     {
         if($lay[$index] -ne "2")
@@ -68,14 +49,14 @@ foreach ($lay in $Layers)
             $UsedIndexes += $index
         }
     }
+	
+	if($UsedIndexes.Count -eq $TotalIndexes.Count)
+	{
+		break
+	}
 }
 
-Write-Host "Total Index Length" $TotalIndexes.Count
-Write-Host "Used Index Length" $UsedIndexes.Count
-
-
-Write-Host "Final" $Final.Count
-
+<#"Draw" Final Layer Overlap#>
 For($i = 0; $i -lt $Final.Count; $i++)
 {
     if($i % $Width  -eq 0)
@@ -85,10 +66,10 @@ For($i = 0; $i -lt $Final.Count; $i++)
 
     if($Final[$i] -eq "0")
     {
-        Write-Host -NoNewLine " "
+        Write-Host -NoNewLine "0" -foregroundcolor black
     }
     else
     {
-        Write-Host -NoNewLine "1"
+        Write-Host -NoNewLine "0" -foregroundcolor white
     }
 }

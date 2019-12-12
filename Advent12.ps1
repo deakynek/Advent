496a33971961000ef778787e1afcd8b85cf2638d
SANDC\karl.deakyne <karl.deakyne@sandc.com>
2019-12-12 15:42:16 -0600

    Cleaning up and making output pretty

--- Advent12.ps1
+++ Advent12.ps1
@@ -66,17 +66,11 @@ function lcmFromArray
 $file = Get-Content Advent12_input.txt
 
 $Planet = @{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}
-<#Part 1#>
-$Planets1 = @(@{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}, `
+$Planets = @(@{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}, `
 			 @{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}, `
 			 @{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}, `
 			 @{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}	)
-			 
-<#Part 2#>
-$Planets2 = @(@{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}, `
-			 @{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}, `
-			 @{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}, `
-			 @{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}	)
+
 			 
 $InitialState = @(@{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}, `
 				@{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}, `
@@ -88,13 +82,9 @@ for($i= 0; $i -lt $file.Count; $i++)
 	$withoutBraces = $file[$i].Substring(1,$file[$i].Length-2)
 	$Parse = $withoutBraces.Split(",=")
 
-	$Planets1[$i]["Position"][0] = [int]($Parse[1])
-	$Planets1[$i]["Position"][1] = [int]($Parse[3])
-	$Planets1[$i]["Position"][2] = [int]($Parse[5])	
-	
-	$Planets2[$i]["Position"][0] = [int]($Parse[1])
-	$Planets2[$i]["Position"][1] = [int]($Parse[3])
-	$Planets2[$i]["Position"][2] = [int]($Parse[5])
+	$Planets[$i]["Position"][0] = [int]($Parse[1])
+	$Planets[$i]["Position"][1] = [int]($Parse[3])
+	$Planets[$i]["Position"][2] = [int]($Parse[5])
 	$InitialState[$i]["Position"][0] = [int]($Parse[1])
 	$InitialState[$i]["Position"][1] = [int]($Parse[3])
 	$InitialState[$i]["Position"][2] = [int]($Parse[5])
@@ -106,55 +96,55 @@ $steps = 1
 while($steps -le 1000)
 {
 	<#Change vel#>
-	for($i = 0; $i -lt $Planets1.Count; $i++)
+	for($i = 0; $i -lt $Planets.Count; $i++)
 	{
-		for($j = 0; $j -lt $Planets1.Count; $j++)
+		for($j = 0; $j -lt $Planets.Count; $j++)
 		{
 			if($i -eq $j)
 			{
 				continue
 			}
 		
-			$PlanetPos1 = $Planets1[$i]["Position"]
-			$PlanetPos2 = $Planets1[$j]["Position"]
+			$PlanetPos1 = $Planets[$i]["Position"]
+			$PlanetPos2 = $Planets[$j]["Position"]
 			
 			if($PlanetPos1[0] -lt $PlanetPos2[0])
 			{
-				$Planets1[$i]["Velocity"][0] += 1
+				$Planets[$i]["Velocity"][0] += 1
 			}
 			elseif ($PlanetPos1[0] -gt $PlanetPos2[0])
 			{
-				$Planets1[$i]["Velocity"][0] -= 1
+				$Planets[$i]["Velocity"][0] -= 1
 			}
 			
 			if($PlanetPos1[1] -lt $PlanetPos2[1])
 			{
-				$Planets1[$i]["Velocity"][1] += 1
+				$Planets[$i]["Velocity"][1] += 1
 			}
 			elseif ($PlanetPos1[1] -gt $PlanetPos2[1])
 			{
-				$Planets1[$i]["Velocity"][1] -= 1
+				$Planets[$i]["Velocity"][1] -= 1
 			}	
 
 			if($PlanetPos1[2] -lt $PlanetPos2[2])
 			{
-				$Planets1[$i]["Velocity"][2] += 1
+				$Planets[$i]["Velocity"][2] += 1
 			}
 			elseif ($PlanetPos1[2] -gt $PlanetPos2[2])
 			{
-				$Planets1[$i]["Velocity"][2] -= 1
+				$Planets[$i]["Velocity"][2] -= 1
 			}
 		}
 	}
 
 	<#Change Pos#>
-	for($i = 0; $i -lt $Planets1.Count; $i++)
+	for($i = 0; $i -lt $Planets.Count; $i++)
 	{
-		$PlanetVel = $Planets1[$i]["Velocity"]
+		$PlanetVel = $Planets[$i]["Velocity"]
 		
-		$Planets1[$i]["Position"][0] += $PlanetVel[0]
-		$Planets1[$i]["Position"][1] += $PlanetVel[1]
-		$Planets1[$i]["Position"][2] += $PlanetVel[2]
+		$Planets[$i]["Position"][0] += $PlanetVel[0]
+		$Planets[$i]["Position"][1] += $PlanetVel[1]
+		$Planets[$i]["Position"][2] += $PlanetVel[2]
 	}
 	
 	$steps++
@@ -162,10 +152,10 @@ while($steps -le 1000)
 
 <#Calc Energy Part 1#>
 $Energy = 0
-for($i = 0; $i -lt $Planets1.Count; $i++)
+for($i = 0; $i -lt $Planets.Count; $i++)
 {
-	$PlanetVel = $Planets1[$i]["Velocity"]
-	$PlanetPos = $Planets1[$i]["Position"]
+	$PlanetVel = $Planets[$i]["Velocity"]
+	$PlanetPos = $Planets[$i]["Position"]
 	
 	$KE = 0
 	$KE += [math]::abs($PlanetVel[0])
@@ -182,6 +172,25 @@ for($i = 0; $i -lt $Planets1.Count; $i++)
 
 Write-Host "Total Energy $Energy after 1000 steps`n`n" -foregroundcolor green
 
+<#Reset#>
+$Planets = @(@{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}, `
+			 @{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}, `
+			 @{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}, `
+			 @{"Position"= @(0,0,0); "Velocity" = @(0,0,0)}	)
+
+for($i= 0; $i -lt $file.Count; $i++)
+{
+	$withoutBraces = $file[$i].Substring(1,$file[$i].Length-2)
+	$Parse = $withoutBraces.Split(",=")
+
+	$Planets[$i]["Position"][0] = [int]($Parse[1])
+	$Planets[$i]["Position"][1] = [int]($Parse[3])
+	$Planets[$i]["Position"][2] = [int]($Parse[5])
+	
+	<#Write-Host "`nPlanet $i : "
+	Write-Host "Position: "($Planets[$i]["Position"][0])","($Planets[$i]["Position"][1])","($Planets[$i]["Position"][2])")"
+	Write-Host "Velocity: "($Planets[$i]["Velocity"][0])","($Planets[$i]["Velocity"][1])","($Planets[$i]["Velocity"][2])")"	#>
+}
 
 
 <#Part 2#>
@@ -203,55 +212,55 @@ while(!($xmatch  -and $ymatch -and $zmatch))
 	}
 
 	<#Change vel#>
-	for($i = 0; $i -lt $Planets2.Count; $i++)
+	for($i = 0; $i -lt $Planets.Count; $i++)
 	{
-		for($j = 0; $j -lt $Planets2.Count; $j++)
+		for($j = 0; $j -lt $Planets.Count; $j++)
 		{
 			if($i -eq $j)
 			{
 				continue
 			}
 		
-			$PlanetPos1 = $Planets2[$i]["Position"]
-			$PlanetPos2 = $Planets2[$j]["Position"]
+			$PlanetPos1 = $Planets[$i]["Position"]
+			$PlanetPos2 = $Planets[$j]["Position"]
 			
 			if($PlanetPos1[0] -lt $PlanetPos2[0])
 			{
-				$Planets2[$i]["Velocity"][0] += 1
+				$Planets[$i]["Velocity"][0] += 1
 			}
 			elseif ($PlanetPos1[0] -gt $PlanetPos2[0])
 			{
-				$Planets2[$i]["Velocity"][0] -= 1
+				$Planets[$i]["Velocity"][0] -= 1
 			}
 			
 			if($PlanetPos1[1] -lt $PlanetPos2[1])
 			{
-				$Planets2[$i]["Velocity"][1] += 1
+				$Planets[$i]["Velocity"][1] += 1
 			}
 			elseif ($PlanetPos1[1] -gt $PlanetPos2[1])
 			{
-				$Planets2[$i]["Velocity"][1] -= 1
+				$Planets[$i]["Velocity"][1] -= 1
 			}	
 
 			if($PlanetPos1[2] -lt $PlanetPos2[2])
 			{
-				$Planets2[$i]["Velocity"][2] += 1
+				$Planets[$i]["Velocity"][2] += 1
 			}
 			elseif ($PlanetPos1[2] -gt $PlanetPos2[2])
 			{
-				$Planets2[$i]["Velocity"][2] -= 1
+				$Planets[$i]["Velocity"][2] -= 1
 			}
 		}
 	}
 
 	<#Change Pos#>
-	for($i = 0; $i -lt $Planets2.Count; $i++)
+	for($i = 0; $i -lt $Planets.Count; $i++)
 	{
-		$PlanetVel = $Planets2[$i]["Velocity"]
+		$PlanetVel = $Planets[$i]["Velocity"]
 		
-		$Planets2[$i]["Position"][0] += $PlanetVel[0]
-		$Planets2[$i]["Position"][1] += $PlanetVel[1]
-		$Planets2[$i]["Position"][2] += $PlanetVel[2]
+		$Planets[$i]["Position"][0] += $PlanetVel[0]
+		$Planets[$i]["Position"][1] += $PlanetVel[1]
+		$Planets[$i]["Position"][2] += $PlanetVel[2]
 	}
 	
 	$thisXMatch = $true
@@ -263,20 +272,20 @@ while(!($xmatch  -and $ymatch -and $zmatch))
 	<#so the number of steps needed to repeat in each dimension are also independent#>
 	for($i= 0; $i -lt $file.Count; $i++)
 	{
-		if($InitialState[$i]["Velocity"][0] -ne $Planets2[$i]["Velocity"][0] -or 
-			$InitialState[$i]["Position"][0] -ne $Planets2[$i]["Position"][0])
+		if($InitialState[$i]["Velocity"][0] -ne $Planets[$i]["Velocity"][0] -or 
+			$InitialState[$i]["Position"][0] -ne $Planets[$i]["Position"][0])
 		{
 			$thisXMatch = $false
 		}
 		
-		if($InitialState[$i]["Velocity"][1] -ne $Planets2[$i]["Velocity"][1] -or `
-			$InitialState[$i]["Position"][1] -ne $Planets2[$i]["Position"][1])
+		if($InitialState[$i]["Velocity"][1] -ne $Planets[$i]["Velocity"][1] -or `
+			$InitialState[$i]["Position"][1] -ne $Planets[$i]["Position"][1])
 		{
 			$thisYMatch = $false
 		}
 		
-		if($InitialState[$i]["Velocity"][2] -ne $Planets2[$i]["Velocity"][2] -or `
-			$InitialState[$i]["Position"][2] -ne $Planets2[$i]["Position"][2])
+		if($InitialState[$i]["Velocity"][2] -ne $Planets[$i]["Velocity"][2] -or `
+			$InitialState[$i]["Position"][2] -ne $Planets[$i]["Position"][2])
 		{
 			$thisZMatch = $false
 		}		
@@ -284,18 +293,46 @@ while(!($xmatch  -and $ymatch -and $zmatch))
 	
 	if(!$xmatch -and $thisXMatch)
 	{
-		Write-Host "X Match at " $steps
+		Write-Host "X Match at " $steps -foregroundcolor blue
 		$xsteps = $steps
+		
+		Write-Host "State at X Match" -foregroundcolor darkgreen
+		for($i= 0; $i -lt $file.Count; $i++)
+		{
+			Write-Host "Planet $i : " -foregroundcolor darkgreen
+			Write-Host "Position: ("($Planets[$i]["Position"][0])","($Planets[$i]["Position"][1])","($Planets[$i]["Position"][2])")" -foregroundcolor darkgreen
+			Write-Host "Velocity: ("($Planets[$i]["Velocity"][0])","($Planets[$i]["Velocity"][1])","($Planets[$i]["Velocity"][2])")"	-foregroundcolor darkgreen
+			Write-Host ""
+		}
 	}
 	if(!$ymatch -and $thisYMatch)
 	{
-		Write-Host "Y Match at " $steps
+		Write-Host "Y Match at " $steps -foregroundcolor blue
 		$ysteps = $steps
+		
+		Write-Host "State at Y Match" -foregroundcolor darkgreen
+		for($i= 0; $i -lt $file.Count; $i++)
+		{
+			Write-Host "Planet $i : " -foregroundcolor darkgreen
+			Write-Host "Position: ("($Planets[$i]["Position"][0])","($Planets[$i]["Position"][1])","($Planets[$i]["Position"][2])")" -foregroundcolor darkgreen
+			Write-Host "Velocity: ("($Planets[$i]["Velocity"][0])","($Planets[$i]["Velocity"][1])","($Planets[$i]["Velocity"][2])")"	-foregroundcolor darkgreen
+			Write-Host ""
+		}		
+		
 	}
 	if(!$zmatch -and $thisZMatch)
 	{
-		Write-Host "Z Match at " $steps
+		Write-Host "Z Match at " $steps -foregroundcolor blue
 		$zsteps = $steps
+		
+		Write-Host "State at Z Match" -foregroundcolor darkgreen
+		for($i= 0; $i -lt $file.Count; $i++)
+		{
+			Write-Host "Planet $i : " -foregroundcolor darkgreen
+			Write-Host "Position: ("($Planets[$i]["Position"][0])","($Planets[$i]["Position"][1])","($Planets[$i]["Position"][2])")" -foregroundcolor darkgreen
+			Write-Host "Velocity: ("($Planets[$i]["Velocity"][0])","($Planets[$i]["Velocity"][1])","($Planets[$i]["Velocity"][2])")"	-foregroundcolor darkgreen
+			Write-Host ""
+		}			
 	}
 	
 	
@@ -309,6 +346,15 @@ while(!($xmatch  -and $ymatch -and $zmatch))
 Write-Host "XSteps: $xsteps steps" 
 Write-Host "YSteps: $ysteps steps" 
 Write-Host "ZSteps: $zsteps steps" 
+
+
+for($i= 0; $i -lt $file.Count; $i++)
+{
+	Write-Host "`nPlanet $i : "
+	Write-Host "Position: "($Planets[$i]["Position"][0])","($Planets[$i]["Position"][1])","($Planets[$i]["Position"][2])")"
+	Write-Host "Velocity: "($Planets[$i]["Velocity"][0])","($Planets[$i]["Velocity"][1])","($Planets[$i]["Velocity"][2])")"	
+}
+
 
 $LcmXYZ = lcmFromArray -array (@($xsteps,$ysteps,$zsteps))
 

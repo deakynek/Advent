3233c77fe9143ed26e8a07244b0e83b5ec23f02b
Karl Deakyne <deakynek@gmail.com>
2020-12-01 14:41:07 -0600

    Finishing Up Advent 1 with more generic code

---
+++ 2020/Advent/Advent1/SumFinder.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace Advent
+{
+    class SumFinder
+    {
+        List<int> entries;
+
+        public SumFinder(List<string> lines)
+        {
+            entries = lines.Select(line => Int32.Parse(line)).ToList<int>();
+        }
+
+        public SumFinder(List<int> entries)
+        {
+            this.entries = entries;
+        }
+
+        public List<int> FindSumWithNNumbers(int sum, int n)
+        {
+            if(entries == null || !entries.Any())
+                return null;
+
+            var numbers = RecurseFindSum(sum, n, 0, 0);
+            return numbers;
+        }
+
+        private List<int> RecurseFindSum(int remainingSum,  int n, int startingIndex, int currentDepth)
+        {
+            if(remainingSum <= 0)
+                return null;
+
+            var index = 0;
+            foreach(int entry in entries.Skip(startingIndex))
+            {
+                if(currentDepth != n-1)
+                {
+                    var depthSum = RecurseFindSum(remainingSum-entry, n, startingIndex+index+1, currentDepth+1);
+                    if(depthSum ==null )
+                    {
+                        index++;
+                        continue;
+                    }
+                    else
+                    {
+                        depthSum.Add(entry);
+                        return depthSum;
+                    }
+                }
+                else
+                {
+                    if(entry == remainingSum)
+                    {
+                        var list = new List<int>();
+                        list.Add(entry);
+                        return list;
+                    }
+                }
+
+                index++;
+            }
+
+            return null;
+        }
+
+        public void PrintMultOfNNumbersSummingToValue(string opText, int sum, int n)
+        {
+            var numberList = FindSumWithNNumbers(sum, n);
+
+            if(numberList == null)
+            {
+                Console.WriteLine(String.Format("{0}: Could not find {1} numbers in file that would sum to {2}",opText,n, sum));
+                return;            
+            }
+
+            var mult = 1;
+            numberList.ForEach(num => mult *= num);
+            var output = opText +": "+ mult.ToString();
+            Console.WriteLine(output);
+        }
+
+    }    
+}
---
+++ 2020/Advent/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+
+namespace Advent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] lines = System.IO.File.ReadAllLines(@".\Advent1\Advent1Input.txt");
+
+            var Finder = new SumFinder(lines.ToList<string>());
+
+            var start = DateTime.UtcNow;
+            Finder.PrintMultOfNNumbersSummingToValue("Sum 2", 2020, 2);
+            PrintElapsedTime("Sum 2", start);
+
+            start = DateTime.UtcNow;
+            Finder.PrintMultOfNNumbersSummingToValue("Sum 3", 2020,3);
+            PrintElapsedTime("Sum 3", start);
+
+            start = DateTime.UtcNow;
+            Finder.PrintMultOfNNumbersSummingToValue("Sum 4", 2020, 4);
+            PrintElapsedTime("Sum 4", start);
+
+            start = DateTime.UtcNow;
+            Finder.PrintMultOfNNumbersSummingToValue("Sum 5", 2020, 5);
+            PrintElapsedTime("Sum 5", start);           
+        }
+
+        static private void PrintElapsedTime(string operation, DateTime start)
+        {
+            var span = DateTime.UtcNow - start;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}.{2:000}",
+                span.Minutes, span.Seconds,
+                span.Milliseconds);
+            Console.WriteLine("RunTime of " +operation+ ": " + elapsedTime);
+        }
+    }
+}

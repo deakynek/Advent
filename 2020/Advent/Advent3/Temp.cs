adbd47a4d92d99f7297908eade901482bc90e874
Karl Deakyne <deakynek@gmail.com>
2020-12-02 09:36:16 -0600

    Cleaning up

---
+++ 2020/Advent/Advent3/Temp.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Advent
+{
+    class Temp
+    {
+
+        public Temp(List<string> lines)
+        {
+        }
+    }    
+}
--- 2020/Advent/Program.cs
+++ 2020/Advent/Program.cs
@@ -17,26 +17,35 @@ namespace Advent
             int day = 2;
             string[] lines = System.IO.File.ReadAllLines(String.Format(@".\Advent{0}\Advent{0}Input.txt",day));
 
-            if(day == 1)
+            switch(day)
             {
-                var Finder = new SumFinder(lines.ToList<string>());
+                case 1:
+                    var Finder = new SumFinder(lines.ToList<string>());
 
-                var start = DateTime.UtcNow;
+                    var start = DateTime.UtcNow;
 
-                var ns = new List<int>(){2,3,4,5,6,7,8,9,10};
-                foreach(var n in ns)
-                {
-                    start = DateTime.UtcNow;
-                    Finder.PrintMultOfNNumbersSummingToValue("Sum "+n.ToString(), 5000, n);
-                    PrintElapsedTime("Sum "+n.ToString(), start);
+                    var ns = new List<int>(){2,3,4,5,6,7,8,9,10};
+                    foreach(var n in ns)
+                    {
+                        start = DateTime.UtcNow;
+                        Finder.PrintMultOfNNumbersSummingToValue("Sum "+n.ToString(), 2020, n);
+                        PrintElapsedTime("Sum "+n.ToString(), start);
 
-                }
-            }
-            else if(day == 2)
-            {
-                var passwordParsed = new PasswordParser(lines.ToList<string>());
-                Console.WriteLine(String.Format("{0} Valid passwords found by range", passwordParsed.FindValidPasswordCountByRange()));
-                Console.WriteLine(String.Format("{0} Valid passwords found by position", passwordParsed.FindValidPasswordCountByPosition()));
+                    }
+                    break;
+
+                case 2: 
+                    var passwordParsed = new PasswordParser(lines.ToList<string>());
+                    Console.WriteLine(String.Format("{0} Valid passwords found by range", passwordParsed.FindValidPasswordCountByRange()));
+                    Console.WriteLine(String.Format("{0} Valid passwords found by position", passwordParsed.FindValidPasswordCountByPosition()));
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    break;
+                
+                default:
+                    break;
             }
         }
 

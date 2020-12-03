174921598bdfc8f352b6396980901e2b05015ecf
BrilliantGambit <deakynek@gmail.com>
2020-12-03 10:39:29 -0600

    Advent 4 - making clean slate

--- 2020/Advent/AdventX/AdventX.cs
+++ 2020/Advent/AdventX/AdventX.cs
@@ -5,11 +5,11 @@ using System.Linq;
 
 namespace Advent
 {
-    class Advent4
+    class AdventX
     {
         List<List<string>> map = new List<List<string>>();
 
-        public Advent4(List<string> lines)
+        public AdventX(List<string> lines)
         {
             
             foreach(var line in lines)
